7cfc94a83b97678feddf944e4be5c2f9cc54eb92
Joey Sabey <joey.sabey@gmx.com>
2024-04-29 10:04:45 +0100

    feature: addded super stubby urfave/cli/v3 setup in main.go

---
+++ main.go
@@ -0,0 +1,26 @@
+// This main package, which builds a 'zelkata' binary, will serve as a simple CLI entry point for creatuing, curating,
+// querying, and generally _using_ the Zelkata system. The urfave/cli package seems to quite nicely supply all the
+// tools I need to not get bogged down in CLI work., and I can focus on the core functionality of the system itself.
+package main 
+
+import (
+   "context"
+   "os"
+
+   _"github.com/omnikron13/zelkata/note"
+
+   "github.com/urfave/cli/v3"
+)
+
+
+// main entry point for the 'top level' command 'zelkata'. Handling is then passed over to urfave/cli. This _may_ be
+// a temporary arrangement, as I'm unsure how much overlap there is likely to be be with my ultimate plan of a modern
+// TUI rather than a 'flags and subcommands' model. I may maintain both though, along with web interfaces, apps, etc.
+func main() {
+   cmd:= &cli.Command{
+      Name:  "Zelkata",
+      Usage: "add notes and stuff",
+   }
+
+   cmd.Run(context.Background(), os.Args)
+}
