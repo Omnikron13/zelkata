bb7a5350550eb7ecc8b5d54735b5ca25cf98690f
Joey Sabey <joey.sabey@gmx.com>
2024-05-04 23:43:17 +0100

    ci: add test & test files for findYAMLFiles()

--- config/config_test.go
+++ config/config_test.go
@@ -5,6 +5,7 @@ import (
    "path/filepath"
    "testing"
 
+   "github.com/adrg/xdg"
    "github.com/stretchr/testify/assert"
 )
 
@@ -13,6 +14,20 @@ import (
 func Test_defaultConfig(t *testing.T) {
    bytes, _ := os.ReadFile("defaults.yaml")
    assert.Equal(t, defaultConfig, bytes)
+}
+
+
+func Test_findYAMLFiles(t *testing.T) {
+   testdata, _ := filepath.Abs("testdata")
+   xdg.ConfigHome = filepath.Join(testdata, "xdg_config_home")
+   xdg.ConfigDirs[0] = filepath.Join(testdata, "xdg_config_dirs_0")
+   xdg.ConfigDirs[1] = filepath.Join(testdata, "xdg_config_dirs_1")
+
+   var yamlFiles []string
+   for _, f := range findYAMLFiles() {
+      yamlFiles = append(yamlFiles, filepath.Base(f))
+   }
+   assert.Equal(t, []string{"config_a.yaml", "config_b.yml", "00_conf.yaml", "01_conf.yml", "config_c.yml", "config_d.yaml", "02_conf.yml", "03_conf.yaml", "config_e.yaml", "config_f.yml", "04_conf.yaml", "05_conf.yml"}, yamlFiles)
 }
 
 
