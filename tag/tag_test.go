be12f9882003621987f241a5744d6c7c8c566465
Joey Sabey <joey.sabey@gmx.com>
2024-05-11 22:40:32 +0100

    format: split tag literal across multiple lines in tag_test.go

--- tag/tag_test.go
+++ tag/tag_test.go
@@ -12,7 +12,14 @@ func Test_LoadPath(t *testing.T) {
    path := filepath.Join("testdata", "test.tag.yaml")
    tag, err := LoadPath(path)
    assert.Nil(t, err)
-   assert.Equal(t, Tag{Name: "Test Tag", Description: "An example tag for testing purposes.", Notes: []string{"QWERTYUIOP", "ASDFGHJKLZ"}}, *tag)
+   assert.Equal(t, Tag{
+      Name: "Test Tag",
+      Description: "An example tag for testing purposes.",
+      Notes: []string{
+         "QWERTYUIOP",
+         "ASDFGHJKLZ",
+      },
+   }, *tag)
 }
 
 
