c6df6c34a025475e63d96c216f72caef1d7c58f5
Joey Sabey <joey.sabey@gmx.com>
2024-05-12 02:46:36 +0100

    feature: add TagMap.Reindex() method

--- tag/tagmap.go
+++ tag/tagmap.go
@@ -6,8 +6,9 @@ import (
    "path/filepath"
 
    "github.com/omnikron13/zelkata/paths"
-)
 
+   "gopkg.in/yaml.v3"
+)
 
 // TagMap is a map of tag names to Tag structs, representing all tags in the system.
 // It includes aliases, and normalises all names before lookup.
@@ -57,5 +58,51 @@ func (m *TagMap) Add(name string, tag *Tag) error {
 // Get returns a reference from the TagMap by name, or nil if it doesn't exist. The name is normalised before lookup.
 func (m *TagMap) Get(name string) *Tag {
    return (*m)[normaliseName(name)]
+}
+
+
+// Reindex clears the Notes field of all tags in the TagMap, then repopulates them by scanning the notes directory.
+func (m *TagMap) Reindex() error {
+   // Clear existing note references
+   for name, tag := range *m {
+      if name != normaliseName(tag.Name) {
+         continue
+      }
+      tag.Notes = []string{}
+   }
+
+   // Read notes and add their IDs to the appropriate tags, creating new tags as necessary
+   files, err := os.ReadDir(paths.Notes())
+   if err != nil {
+      return err
+   }
+   for _, file := range files {
+      // TODO: rework Note/Meta so they can be unmarshalled into properly and replace this hacky generic map
+      note := map[string]any{}
+      b, err := os.ReadFile(filepath.Join(paths.Notes(), file.Name()))
+      if err != nil {
+         return err
+      }
+      if err := yaml.Unmarshal(b, note); err != nil {
+         return err
+      }
+      tags, ok := note["tags"].([]any)
+      if !ok {
+         return errors.New("Tags field is not an array")
+      }
+      for _, t := range tags {
+         t, ok := t.(string)
+         if !ok {
+            return errors.New("Tag is not a string")
+         }
+         tag := m.Get(t)
+         if tag == nil {
+            tag = &Tag{Name: t, Notes: []string{}}
+            _ = m.Add(t, tag)
+         }
+         tag.Notes = append(tag.Notes, note["id"].(string))
+      }
+   }
+   return nil
 }
 
