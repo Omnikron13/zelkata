b65bad02b32ff8112a1598e4ebf1e4f066baa939
Joey Sabey <joey.sabey@gmx.com>
2024-05-19 10:15:44 +0100

    tests: add meta_tes.go & Test_MarshalYAML()

---
+++ note/meta_test.go
@@ -0,0 +1,43 @@
+package note
+
+import (
+   "testing"
+
+   "github.com/stretchr/testify/assert"
+   "gopkg.in/yaml.v3"
+)
+
+func Test_MarshalYAML(t *testing.T) {
+   t.Run("simple meta", func(t *testing.T) {
+      meta := Meta {
+         ID: "123456789",
+         Tags: []string{"Foo", "Bar"},
+         Created: "2024-05-13T01:02:03Z00:00",
+      }
+      data, err := yaml.Marshal(&meta)
+      assert.Nil(t, err)
+      assert.Equal(t, "created: 2024-05-13T01:02:03Z00:00\nid: \"123456789\"\ntags:\n    - Foo\n    - Bar\n", string(data))
+   })
+
+   t.Run("complex meta", func(t *testing.T) {
+      modified := "2024-05-13T01:02:03Z00:00"
+      title := "Test Note"
+      format := "AsciiDoc"
+      meta := Meta {
+         ID: "123456789",
+         Tags: []string{"Foo", "Bar"},
+         Created: "2024-05-13T01:02:03Z00:00",
+         Modified: &modified,
+         Refs: &map[string]string{
+            "Website": "https://example.com",
+            "Book": "ISBN 1234567890",
+         },
+         Format: &format,
+         Title: &title,
+      }
+      data, err := yaml.Marshal(&meta)
+      assert.Nil(t, err)
+      assert.Equal(t, "created: 2024-05-13T01:02:03Z00:00\nformat: AsciiDoc\nid: \"123456789\"\nmodified: 2024-05-13T01:02:03Z00:00\nrefs:\n    Book: ISBN 1234567890\n    Website: https://example.com\ntags:\n    - Foo\n    - Bar\ntitle: Test Note\n", string(data))
+   })
+}
+
